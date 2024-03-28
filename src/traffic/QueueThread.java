f239094d10714b81586e6646c3c5f7794962d8bd
Rutzno <d.rutzno@gmail.com>
2024-03-28 17:48:17 +0000

    stage 5

--- src/traffic/QueueThread.java
+++ src/traffic/QueueThread.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 /**
  * @author Mack_TB
  * @since 23/03/2024
- * @version 1.0.4
+ * @version 1.0.5
  */
 
 public class QueueThread implements Runnable{
-    Scanner scanner = new Scanner(System.in);
     private int passedTime = 0;
     private int numberOfRoads;
     private int intervals;
+    private CircularQueue circularQueue;
 
     public QueueThread(int numberOfRoads,
                        int intervals) {
         this.numberOfRoads = numberOfRoads;
         this.intervals = intervals;
+        circularQueue = new CircularQueue(numberOfRoads);
     }
 
     @Override
@@ -28,6 +29,7 @@ public class QueueThread implements Runnable{
                 System.out.println("! " + passedTime + "s. have passed since system startup !");
                 System.out.printf("! Number of roads: %d !%n", numberOfRoads);
                 System.out.printf("! Interval: %d !%n", intervals);
+                circularQueue.printRoads();
                 System.out.println("! Press \"Enter\" to open menu !");
             }
             try {
@@ -36,8 +38,12 @@ public class QueueThread implements Runnable{
                 throw new RuntimeException(e);
             }
             if (TrafficLights.modeSystem) {
-                TrafficLights.clearConsole(Main.sc, false);
+                TrafficLights.clearConsole(false);
             }
         } while (!TrafficLights.finished);
+    }
+
+    public CircularQueue getCircularQueue() {
+        return circularQueue;
     }
 }
