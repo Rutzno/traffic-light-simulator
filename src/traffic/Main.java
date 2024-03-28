c5d26c4cc036809c240ae3d2138099fd7d4bddb4
Rutzno <d.rutzno@gmail.com>
2024-03-28 17:12:20 +0000

    initial commit

---
+++ src/traffic/Main.java
@@ -0,0 +1,19 @@
+package traffic;
+
+/**
+ * @author Mack_TB
+ * @since 23/03/2024
+ * @version 1.0.0
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the traffic management system!");
+        System.out.println("""
+            Menu:
+            1. Add
+            2. Delete
+            3. System
+            0. Quit""");
+    }
+}
